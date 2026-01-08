e27128aacd68df27a44547c165fafe7473c7fd7a
Carly Ramacher <Carly.Ramacher@jonesctr.org>
2026-01-08 14:37:26 -0500

    bark bark finished

--- teambark.R
+++ teambark.R
@@ -1,4 +1,5 @@
 #loading libraries
+install.packages("tidyverse")
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
@@ -10,24 +11,29 @@ ltm <- read.csv("R:/landscape_ecology/codeblitz/codeblitz2/ltm-data.csv")
 calc_qmd <- function(dbh,dbh_threshold=10) {
   if(!is.numeric(dbh)) warning("dbh must be numeric.")
   
-  dbh_cm <- dbh[!is.na(dbh) & dbh>=dbh_threshold] #might not work!! in progress
+  dbh_cm <- dbh[!is.na(dbh)]
+  dbh_cm <- dbh_cm[dbh_cm >= dbh_threshold]
   qmd <- sqrt(mean(dbh_cm^2))
   
   return(qmd)
 }
+calc_qmd(dbh)
 
+#calculating qmd by year
 
-#graphing avg qmd by year for all plots
 qmd_by_yr <- ltm %>% group_by(year, plotId) %>% 
   summarise(qmd = calc_qmd(dbh))
+qmd_by_yr_filtered <- qmd_by_yr %>%
+  filter(!is.na(qmd))
 
-qmd <- qmd_by_yr %>% group_by(year) %>% summarise(mean_qmd = mean(qmd))
+avg_by_yr <- qmd_by_yr_filtered %>% group_by(year) %>% summarise(avgqmd = mean(qmd)) 
+
+#graphing avg qmd by year for all plots
+
 ggplot(data = qmd, aes(x = year, y = mean_qmd)) +
   geom_point() +
   scale_x_continuous(breaks = seq(from = 2000, to = 2025, by = 2)) +
   theme_bw()
-
-
 
 ltm_summary %>% ggplot(aes(qmd))+
   geom_histogram()+
